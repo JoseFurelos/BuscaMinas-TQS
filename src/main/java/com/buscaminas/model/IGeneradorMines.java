77abb479b68c05d020ace35b60e4c5acd6279e82
JoseFurelos <1673379@uab.cat>
2025-12-03 12:33:18 +0100

    mock GeneradorMines

---
+++ src/main/java/com/buscaminas/model/IGeneradorMines.java
@@ -0,0 +1,5 @@
+package com.buscaminas.model;
+
+public interface IGeneradorMines {
+    boolean hiHaMina(int x, int y);
+}
---
+++ src/test/java/com/buscaminas/model/GeneradorMinesTest.java
@@ -0,0 +1,14 @@
+package com.buscaminas.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeneradorMinesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
